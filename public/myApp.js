35da7e7cb53a6313fdc667552b1916c29c43cc02
JacquelineJia <jackiesisy@gmail.com>
2017-01-08 21:39:57 -0500

    added comment function on comments page

--- public/myApp.js
+++ public/myApp.js
@@ -1,7 +1,25 @@
 var app = angular.module('myApp', ['ngAnimate']);
 
-app.controller('myCtrl', function ($scope) {
+app.controller('myCtrl', function ($scope, $http) {
   $scope.backgroundState= "background-image grayscale";
   $scope.headerState= "name-header pink hover";
   $scope.menuState = "menu hid";
+
+  var refresh = function() {
+    $http.get('/commentlist').then(function(response) {
+      // console.log(response.data);
+      $scope.commentlist = response.data;
+      $scope.comment = null;
+    });
+  };
+
+  refresh();
+
+  $scope.addComment = function() {
+    $http.post('/commentlist', $scope.comment).then(function(response) {
+      // console.log(response);
+      refresh();
+    });
+  };
+
 });
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('commentlist', ['commentlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+app.get('/commentlist', function (req, res) {
+  db.commentlist.find(function (err, docs) {
+    // console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/commentlist', function (req, res) {
+  // console.log(req.body);
+  db.commentlist.insert(req.body, function (err, doc) {
+    res.json(doc);
+  });
+});
 
 app.listen(3000);
-
-
